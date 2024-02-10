e1889fba75d5371414ca995bc4469f1267e87436
davvikq <davikversion@gmail.com>
2024-02-10 18:25:38 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+// const colors = ['red', 'green', 'blue', 'yellow']
+
+// for (let i = 0; i < colors.length; i++) {
+//     console.log(colors[i]);
+// }
+
+// let j = 1;
+
+// do {
+//     j++
+//     console.log(j ** j);
+// } while (j <= 10)
+
+// for (let i = 10; i > j; i++) {
+    // console.log(i**i);
+// }
+
+// let num = 20
+// for (let i = 0; i < num; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i);
+//     }
+// }
